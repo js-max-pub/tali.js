98e50681012c5afc7c70060d63e56deb343a1faa
Max Fechner <max.fechner@gmail.com>
2022-04-07 17:09:38 +0200

    mod.js

---
+++ mod.js
@@ -0,0 +1,6 @@
+import { parse } from './grid/parse.js'
+import { stringify } from './grid/stringify.js'
+
+export default {
+	grid: { parse, stringify }
+}
--- test/test.js
+++ test/test.js
@@ -1,14 +1,15 @@
-import { parse } from '../grid/parse.js'
-import { stringify } from '../grid/stringify.js'
+// import { parse } from '../grid/parse.js'
+// import { stringify } from '../grid/stringify.js'
+import TALI from '../mod.js'
 
 if (import.meta.main) { // tests
 	let json, tali
 
 	tali = Deno.readTextFileSync('test1.tali')
-	json = parse(tali, { none: '-' })
+	json = TALI.grid.parse(tali, { none: '-' })
 	console.log(json)
 
 	json = JSON.parse(Deno.readTextFileSync('test1.json'))
-	tali = stringify(json, { none: '-' })
+	tali = TALI.grid.stringify(json, { none: '-' })
 	console.log(tali)
 }
