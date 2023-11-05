cb5bbb323d886a226289babfdc69f1e3985f5028
Maximilian Fechner <max.fechner@gmail.com>
2023-11-05 10:29:58 +0100

    ...

--- deps.js
+++ deps.js
@@ -1,5 +1,5 @@
 // export { blocks, lines, trim } from '../std/string.js'
-export { blocks, lines, trim } from 'https://jsv.max.pub/std/2023/string.js'
+export { blocks, lines, trim } from 'https://jsv.max.pub/std/2023-11/string.js'
 
 // import { Log } from '../../log/mod.js'
 // export const log = new Log('TALI')
